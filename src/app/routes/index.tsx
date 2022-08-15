a3c89e01f14fac054a64eb416ef5310f32272f9f
KevinAlves55 <programador.kevin4773@gmail.com>
2022-08-15 16:41:03 -0300

    feat: Adicionado a troca de tema

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,12 +1,22 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+
+    const { themeName, toggleTheme } = useAppThemeContext();
 
     return(
 
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Doar</Button>} />
+            <Route 
+                path="/pagina-inicial" 
+                element= { 
+                    <Button variant="contained" color="primary" onClick={toggleTheme}>
+                        toggle Theme
+                    </Button> 
+                } 
+            />
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
