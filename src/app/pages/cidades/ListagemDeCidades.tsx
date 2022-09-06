a79c5fd60b0d6573a44d12ff0e6a65f5b0115fd8
KevinAlves55 <programador.kevin4773@gmail.com>
2022-09-06 16:59:57 -0300

    fix: Ajustando o tamanho da TableCell de ações

--- src/app/pages/cidades/ListagemDeCidades.tsx
+++ src/app/pages/cidades/ListagemDeCidades.tsx
@@ -114,7 +114,7 @@ export const ListagemDeCidades: React.FC = () => {
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Ações</TableCell>
+                            <TableCell width="100">Ações</TableCell>
                             <TableCell>Nome</TableCell>
                         </TableRow>
                     </TableHead>
